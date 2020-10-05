fa52a38ad6ab71b962ff72833a2b08d9099fc45c
Minh-Trang Pham <minhtrangvietpham@gmail.com>
2020-10-05 18:21:37 +0700

    Restart the prj

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        DictionaryCommandLine cmd = new DictionaryCommandLine();
-        //cmd.dictionaryFile();
-        cmd.dictionaryBasic();
-    }
-}
